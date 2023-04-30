47ad07fc40da24c835350109692d706d7767134e
belhassen1-jpg <belhassen.knani@esprit.tn>
2023-04-30 16:57:20 +0200

    test

--- projetNimbus/src/main/java/com/example/projetnimbus/DAO/Entities/PaymentFormality.java
+++
@@ -1,6 +0,0 @@
-package com.example.projetnimbus.DAO.Entities;
-
-public enum PaymentFormality {
-    CASH,
-    BANKCARD
-}
--- projetNimbus/src/main/java/com/example/projetnimbus/DAO/Repositories/CommandeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetnimbus.DAO.Repositories;
-
-import com.example.projetnimbus.DAO.Entities.Commande;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommandeRepository extends JpaRepository <Commande,Integer> {
-}
